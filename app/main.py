376feb8452bdde30dda6676d2d5b1005fc3d7ac7
Ishan Shah <ishah8840@gmail.com>
2025-07-23 17:05:46 -0400

    Using sqlalchemy now

--- app/main.py
+++ app/main.py
@@ -76,10 +76,9 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id),))
+def get_post(id: int, db: Session = Depends(get_db)):
 
-    post = cursor.fetchone()
+    post = db.get(models.Post, id)
 
     if not post:
         raise HTTPException(
@@ -91,34 +90,33 @@ def get_post(id: int):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    deleted_post = cursor.fetchone
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.get(models.Post, id)
 
-    if deleted_post == None:
+    if post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id: {id} does not exist",
         )
 
+    db.delete(post)
+    db.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """,
-        (post.title, post.content, post.published, str(id)),
-    )
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    existing_post = post_query.first()
 
-    updated_post = cursor.fetchone()
-    conn.commit()
-
-    if updated_post == None:
+    if existing_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist",
         )
 
-    return {"post": updated_post}
+    post_query.update(post.model_dump(), synchronize_session=False)
+    db.commit()
+
+    return {"post": post_query.first()}
